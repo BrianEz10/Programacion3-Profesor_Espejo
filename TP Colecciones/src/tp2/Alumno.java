7c342c93a1c476f2969339b9db356c07c60bd64c
BrianEz10 <pabrianlucas@gmail.com>
2025-09-14 17:43:50 -0300

    Add files via upload

---
+++ TP Colecciones/src/tp2/Alumno.java
@@ -0,0 +1,16 @@
+package tp2;
+
+public class Alumno {
+    private String nombre;
+    private double nota;
+
+    public Alumno(String nombre, double nota) {
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno { nombre='" + nombre + "', nota=" + nota + " }";
+    }
+}
